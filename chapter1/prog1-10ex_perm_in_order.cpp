c2994a5ba9366afd6af985d0db11244b6113607f
Laevablade-VSCode <wangtif@126,com>
2020-01-12 23:49:01 +0800

    NEW: Added ostream switch

--- chapter1/prog1-10ex_perm_in_order.cpp
+++ chapter1/prog1-10ex_perm_in_order.cpp
@@ -9,11 +9,11 @@ void swap(T& a,T& b)
 }
 
 template <typename T>
-void show_permutation(T* list,int l,int r)
+void show_permutation(T* list,int l,int r,std::ostream& out = std::cout)
 {
     for (int i=l;i<=r;i++)
-        std::cout << list[i] << ' ';
-    std::cout << '\n';
+        out << list[i] << ' ';
+    out << '\n';
 }
 
 //Reverse all elements between a[l] and a[r]
@@ -30,10 +30,10 @@ void reverse(T* list,int l,int r)
 }
 
 //Generate all permutations between list[l] and list[r]. 
-//In lexicographic order.
+//In lexicographic order. (Narayana algorithm)
 //TIPS: C++ STL has such a function named next_permutation(list+l,list+r),
 //which helps to make an easier version of this.
-//this function is written only to show how it works.
+//This function is written only to show how it works.
 template <typename T>
 void permutation(T *list,int l,int r)
 {
@@ -52,16 +52,24 @@ void permutation(T *list,int l,int r)
             //Change list[r] first, then list[r-1]...
             permutation(list,l+1,r);
             
-            //
+            //As assumed above, list[l+1]~list[r] is in "smaller" order
+            //Now, reverse list[l+1]~list[r] to get a "bigger" order
             reverse(list,l+1,r);
+            //Find an element list[k] which is just bigger than list[l]
+            //PS: list[l]>list[k] will generate all permutations in decreasing order.
             int k=l+1;
-            while (k<=r && list[l]>list[k])
+            while (k<=r && list[l]<list[k])
                 k++;
             if (k>r)
                 continue;
+            //Make list[k] the next beginning of the permutation
             swap(list[l],list[k]);
         }
     }
+    //Notice that after reversing list[l+1]~list[r], 
+    //these elements are all in "biggest" order.
+    //Need to reverse list[l+1]~list[r] back
+    //to maintain the assumption above.
     reverse(list,l+1,r);
 }
 
